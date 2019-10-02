c5119b50e09a8876d838643a5ea15a4abf8b9df8
marianolarre <marianolarrete@gmail.com>
2019-10-02 15:36:04 -0300

    Pantalla Crear Carrera

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // En el navegador, apretar F12 para ver la Consola
 console.log("Hola mundo")
+
+// Con JQuery, todo lo que es inicializacion se hace dentro de este 'ready', que se llama cuando el documento esta listo
+$(document).ready(function(){
+
+});
