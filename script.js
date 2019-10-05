47ae4ac64c7d34091af7237006d00398a39ab5ac
marianolarre <marianolarrete@gmail.com>
2019-10-05 18:59:17 -0300

    Ejemplo de boton con funcion

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ console.log("Hola mundo")
 // Con JQuery, todo lo que es inicializacion se hace dentro de este 'ready', que se llama cuando el documento esta listo
 $(document).ready(function(){
 
+	// Para asignarle una función a un botón se debe usar JQuery. $("#id").click(funcion)
+	$("#boton-crear").click(Prueba)
 });
+
+function Prueba() {
+	alert("Prueba")
+}
