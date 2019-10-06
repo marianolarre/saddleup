b24b1105a32fc3dd3bfddef01c10916812c31191
MelodRAMAtick <melodramatick@users.noreply.github.com>
2019-10-05 21:45:32 -0300

    Alert correcto

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // En el navegador, apretar F12 para ver la Consola
 console.log("Hola mundo")
-
+var textoLugar = ""
+var textoHora= ""
 // Con JQuery, todo lo que es inicializacion se hace dentro de este 'ready', que se llama cuando el documento esta listo
 $(document).ready(function(){
-    var texto = $("#labelLugar").html()    
     $("#boton-crear").click(Prueba)
 });
 
 function Prueba() {
-    alert(texto)
+    textoLugar = $("#labelLugar").val()
+    textoHora= $("#labelHora").val()
+    alert(textoHora + " - " + textoLugar)
 }
