ef7b5399a8888669aa5e4b291dfc0958c51fdb52
MelodRAMAtick <melodramatick@users.noreply.github.com>
2019-10-05 21:09:26 -0300

    Extraer texto
    Intento ver cómo extraer el contenido de uno de los cuadros de Input, pero el Alert ni siquiera se ejecuta cuando le paso una variable "texto" que debería recibir de antemano el contenido de su input

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ console.log("Hola mundo")
 
 // Con JQuery, todo lo que es inicializacion se hace dentro de este 'ready', que se llama cuando el documento esta listo
 $(document).ready(function(){
-    //Mi idea es obtener el URL y buscar la posición, en lo que sería su cadena de caracteres, del signo de pregunta,
-    //que es a partir de ahí de donde se pueden agregar parámetros si mal no entiendo. Entonces,  
-    function hacerURL(hora, lugar){
-        var url = tostring(window.location.href);
-        url
-    }
+    var texto = document.getElementById("#labelLugar").textContent;
+    $("#boton-crear").click(Prueba);
 });
+
+function Prueba() {
+    alert(texto);
+}
