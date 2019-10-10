c29112015b494a8802a469b4942d4899d7713d45
Seba-Barco <51105072+Seba-Barco@users.noreply.github.com>
2019-10-10 11:25:37 -0300

    Boton llegada finalizado
    El boton de llegada permite determinar si llegaste exactamente a tiempo o de lo contrario con cuanta anticipación o demora llegaste al evento. Esto lo comunica por medio de una alerta

--- scriptCarreraActual.js
+++ scriptCarreraActual.js
@@ -9,10 +9,45 @@ $(document).ready(function(){
     $("#display-hora").html(decodeURIComponent(Cookies.get('hora')))
     $("#display-lugar").html(decodeURIComponent(Cookies.get('lugar')))
     $("#boton-llegada").click(function(){
-    	var horaEvento=moment(Cookies.get('hora'), "hh:mm");
-    	var hora2=moment();
-    	var diferencia=hora2.diff(horaEvento,"minutes");
-    	console.log(diferencia/60);
-    	alert("Llegaste!\nDiferencia en horas: "+ diferencia/60 + " h");
+
+        //este código esta hecho con un límite de anticipación y demora de 24 horas
+        //Consigo la hora del evento y mi hora de llegada
+        
+        //hora del evento
+        var x=moment(Cookies.get('hora'), "hh:mm");
+
+        //la próxima linea es para testeo local
+        //var x=moment("20:30", "hh:mm");
+
+        //hora de llegada
+        var y=moment();
+
+        //consigo los valores en horas, minutos, segundos y milisegundos, los paso a valores
+        //absolutos (positivos) y a formato adecuado. Ejemplo: si tengo -75 minutos en
+        //realidad tengo 1 hora y 15 minutos de anticipación
+        var diffHours=Math.abs(y.diff(x,"hours"));
+        var diffMinutes=Math.abs(y.diff(x,"minutes"));
+        //var diffSeconds=Math.abs(y.diff(x,"seconds"));
+        //var diffMilliseconds=Math.abs(y.diff(x,"milliseconds"));
+        while(diffMinutes>60){
+            diffMinutes-=60;
+        }
+        /*
+        while(diffSeconds>60){
+            diffSeconds-=60;
+        }
+        while(diffMilliseconds>1000){
+            diffMilliseconds-=1000;
+        }*/
+        if(diffHours == 0 && diffMinutes == 0 && diffSeconds ==0){
+            alert("Llegaste perfectamente a tiempo");
+        }
+        else {
+            var tiempo="demora";
+            if(y.diff(x,"milliseconds")<0){
+                tiempo="anticipación";
+            }
+            alert("Llegaste con una " + tiempo + " de " + diffHours + " h " + diffMinutes + " min");
+        }
     })
 });
