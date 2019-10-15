f727dadc1a4cb0205f5347e05e77898414795c69
lulamasini <51343208+lulamasini@users.noreply.github.com>
2019-10-15 17:27:54 -0300

    Create scriptCrearTienda.js

---
+++ scriptCrearTienda.js
@@ -0,0 +1,9 @@
+
+// Con JQuery, todo lo que es inicializacion se hace dentro de este 'ready', que se llama cuando el documento esta listo
+$(document).ready(function(){
+    $("#boton-comprar").click(leerInputs)
+});
+
+function leerInputs() {
+
+}
