435c4872674965d7e116547d5605e7929487c85f
lulamasini <51343208+lulamasini@users.noreply.github.com>
2019-10-16 09:41:08 -0300

    Colorear Caballo

--- scriptCrearTienda.js
+++ scriptCrearTienda.js
@@ -10,13 +10,25 @@ var colores = [
 
 // Con JQuery, todo lo que es inicializacion se hace dentro de este 'ready', que se llama cuando el documento esta listo
 $(document).ready(function(){
-    $("#boton-comprar").click(leerInputs)
+
     
     for(var i = 0; i < colores.length; i++) {
-           $("#container-botones").append("<button class='boton-color' style='background-color: "+colores[i]+"'>:)</button>");
+           $("#container-botones").append("<button class='boton-color' data-color='"+i+"' style='background-color: "+colores[i]+"'>:)</button>");
     }
+
+	$(".boton-color").click(seleccionarColor);
+
+	pintarCaballo(Cookies.get("color"));
 });
 
-function leerInputs() {
+function seleccionarColor() {
+
+var c = $(this).data("color");
+pintarCaballo(c);
+Cookies.set("color", c);
+}
+
+function pintarCaballo(indiceColor) {
+	$("#caballo").css("background-color", colores[indiceColor]);
 
 }
