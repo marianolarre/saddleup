309d79cec44e30f10e8125dbaf786ef63a3025d8
lulamasini <51343208+lulamasini@users.noreply.github.com>
2019-10-16 09:16:57 -0300

    Prueba botones de tienda

--- scriptCrearTienda.js
+++ scriptCrearTienda.js
@@ -1,7 +1,20 @@
+var colores = [
+    "red",
+    "blue",
+    "green",
+    "cyan",
+    "white",
+    "yellow",
+    "orange"
+]
 
 // Con JQuery, todo lo que es inicializacion se hace dentro de este 'ready', que se llama cuando el documento esta listo
 $(document).ready(function(){
     $("#boton-comprar").click(leerInputs)
+    
+    for(var i = 0; i < colores.length; i++) {
+           $("#container-botones").append("<button class='boton-color' style='background-color: "+colores[i]+"'>:)</button>");
+    }
 });
 
 function leerInputs() {
