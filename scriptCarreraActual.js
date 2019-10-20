c8f3dd6b5daf766100c36640b32af721de505260
Seba-Barco <51105072+Seba-Barco@users.noreply.github.com>
2019-10-20 03:13:52 -0300

    Boton Llegada Finalizado + limites de tiempo
    El boton de llegada maneja perfectamente las fechas del evento y de llegada (toma en cuenta año, mes, dia, hora y minutos). Se tiene en cuenta un limite de anticipación y demora para poder tocar el boton llegar, de modo que solo se recibe recompensa al llegar dentro del rango horario propuesto. Las recompensas no estan implementadas, solo hay comentarios en donde se deben implementar.
    
    Nota: se podría agregar la opción de determinar un rango horario para la llegada al evento al crear la carrera

--- scriptCarreraActual.js
+++ scriptCarreraActual.js
@@ -9,45 +9,62 @@ $(document).ready(function(){
     $("#display-hora").html(decodeURIComponent(Cookies.get('hora')))
     $("#display-lugar").html(decodeURIComponent(Cookies.get('lugar')))
     $("#boton-llegada").click(function(){
-
-        //este código esta hecho con un límite de anticipación y demora de 24 horas
-        //Consigo la hora del evento y mi hora de llegada
+        var x=moment(Cookies.get('fecha')+"T"+Cookies.get('hora'));
         
-        //hora del evento
-        var x=moment(Cookies.get('hora'), "hh:mm");
+        //proxima línea --> testeo local sin cookies
+        //var x = moment("2019-10-20"+"T"+"03:04");
 
-        //la próxima linea es para testeo local
-        //var x=moment("20:30", "hh:mm");
-
-        //hora de llegada
-        var y=moment();
+        //paso la fecha actual al mismo formato que la fecha del evento
+        var year = moment().format("YYYY");
+        var month = moment().format("MM");
+        var day = moment().format("DD");
+        var hours = moment().format("HH");
+        var minutes = moment().format("mm");
+        var y = moment(year + "-" + month + "-" + day + "T" + hours + ":" + minutes);
 
         //consigo los valores en horas, minutos, segundos y milisegundos, los paso a valores
         //absolutos (positivos) y a formato adecuado. Ejemplo: si tengo -75 minutos en
         //realidad tengo 1 hora y 15 minutos de anticipación
         var diffHours=Math.abs(y.diff(x,"hours"));
         var diffMinutes=Math.abs(y.diff(x,"minutes"));
-        //var diffSeconds=Math.abs(y.diff(x,"seconds"));
-        //var diffMilliseconds=Math.abs(y.diff(x,"milliseconds"));
+
         while(diffMinutes>60){
             diffMinutes-=60;
         }
-        /*
-        while(diffSeconds>60){
-            diffSeconds-=60;
-        }
-        while(diffMilliseconds>1000){
-            diffMilliseconds-=1000;
-        }*/
-        if(diffHours == 0 && diffMinutes == 0 && diffSeconds ==0){
+
+        //limite define con cuanto tiempo de anticipación o demora (en minutos) puedo llegar para activar el boton de llegada
+        var limite = 30;
+        var tiempo = "demora";
+        var limiteRecompensa = limite;
+        var recompensa = 100;
+
+        //console.log(limiteRecompensa-diffMinutes);
+
+        if(diffHours == 0 && diffMinutes<limite){
+            if(diffMinutes == 0)
+            {
             alert("Llegaste perfectamente a tiempo");
-        }
-        else {
-            var tiempo="demora";
-            if(y.diff(x,"milliseconds")<0){
-                tiempo="anticipación";
             }
-            alert("Llegaste con una " + tiempo + " de " + diffHours + " h " + diffMinutes + " min");
+            else{
+                if(y.diff(x,"minutes")<0){
+                    tiempo="anticipación";
+                    //recompensa por anticipación
+                }
+                alert("Llegaste con una " + tiempo + " de " + diffMinutes + " min.");
+                //recompensa por demora
+            }
         }
+        else{
+            if(y.diff(x,"minutes")<0){
+                alert("Es muy temprano");
+            }
+            else{
+                alert("Es muy tarde");
+            }
+        }
+
+        //Recompensas
+
+        
     })
 });
