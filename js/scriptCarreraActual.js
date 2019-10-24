b223bf70a58d8ed61bedf073b5e45febda8eb469
Seba-Barco <51105072+Seba-Barco@users.noreply.github.com>
2019-10-24 01:04:03 -0300

    function terminarCarrera()

--- js/scriptCarreraActual.js
+++ js/scriptCarreraActual.js
@@ -20,8 +20,14 @@ $(document).ready(function(){
         var limite = 30;
         calcularTiempoLlegada(fechaHoraEvento, fechaHoraActual, limite);
         recompensar(fechaHoraEvento, fechaHoraActual, limite);
+        terminarCarrera();
     })
 });
+
+function terminarCarrera(){
+    Cookies.set('fecha', Cookies.get('fecha')+ ";expires=Mon, 01 Jan 2000 01:00:00 UTC;")
+    console.log(Cookies.get('fecha')+ ";expires=Mon, 01 Jan 2000 01:00:00 UTC;")
+}
 
 function formatearFechaHoraEvento(){
     return moment(Cookies.get('fecha')+"T"+Cookies.get('hora'));
