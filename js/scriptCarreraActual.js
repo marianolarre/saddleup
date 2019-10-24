89745c597ffaccece2389e930c24ea2db91f50a0
Seba-Barco <51105072+Seba-Barco@users.noreply.github.com>
2019-10-24 03:17:37 -0300

    test3

--- js/scriptCarreraActual.js
+++ js/scriptCarreraActual.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
     recompensar(fechaHoraEvento, fechaHoraActual, limite);
     //eliminarCookies();
 })
-    $("#boton-abandonar").click(eliminarCookies())
+    //$("#boton-abandonar").click(eliminarCookies())
 
 });
 
