3ac4fefe132554147e75b56fe00edf575df955f6
Seba-Barco <51105072+Seba-Barco@users.noreply.github.com>
2019-10-24 02:49:20 -0300

    fix2

--- js/scriptCarreraActual.js
+++ js/scriptCarreraActual.js
@@ -8,12 +8,8 @@ $(document).ready(function(){
     $("#display-fecha").html(decodeURIComponent(Cookies.get('fecha')))
     $("#display-hora").html(decodeURIComponent(Cookies.get('hora')))
     $("#display-lugar").html(decodeURIComponent(Cookies.get('lugar')))
-    $("#boton-llegada").click(llegar())
-    $("#boton-abandonar").click(eliminarCookies())
-
-});
-
-function llegar(){
+    $("#boton-llegada").click(function(){
+        //proxima línea --> testeo local sin cookies
     //var fechaHoraEvento = moment("2019-10-22"+"T"+"15:20");
 
     //paso la fecha del evento al formato de moment.js (creo un objeto moment)
@@ -25,7 +21,10 @@ function llegar(){
     calcularTiempoLlegada(fechaHoraEvento, fechaHoraActual, limite);
     recompensar(fechaHoraEvento, fechaHoraActual, limite);
     eliminarCookies();
-}
+})
+    //$("#boton-abandonar").click(eliminarCookies())
+
+});
 
 function eliminarCookies(){
     Cookies.remove('fecha');
